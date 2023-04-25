9b80145fc790ea1ee526bc8abb6bfa3b6615b33f
tore aldemir <aldemirtore@gmai.com>
2023-04-25 14:55:01 +0300

    end of the player vs player part

--- script.js
+++ script.js
@@ -55,19 +55,68 @@ const gameBoard =(() => {
             });
 
         (xCount >= 3) ? winner = 'x' : (oCount >= 3) ? winner = 'o' : winner;
-
-        if(xCount >= 3 || oCount >= 3){
-            condition.forEach(index => {
-                _boxes[index].classList.add('win-box');
-            })
-        }
         });
+
         return winner;
     }
 
+    const _makeMove = (box_index) => {
+        if(_boardArray[box_index] == '' && turn < 9 && _gameWinner == '') {
+            if(turn %2 == 0) {
+                _boardArray[box_index] = _playerOne.playerShape;
+                _message.textContent = `Player ${_playerTwo.playerShape}'s turn`;
+            }
+            else {
+                _boardArray[box_index] = _playerTwo.playerShape;
+                _message.textContent = `Player ${_playerOne.playerShape}'s turn`;
+            }
+
+            turn++;
+
+            renderBoard();
+
+            _gameWinner = _checkWinner();
+
+            if(_gameWinner != '') _announceWinner(_gameWinner);
+        }
+    }
+
+    const _announceWinner = (winner) => {
+        if(winner == _playerOne.playerShape || winner == _playerTwo.playerShape)
+            _message.textContent = `Player ${winner} has won!`;
+        else
+            _message.textContent = `It's a draw!`;
+    }
+
+    const _restartGame = () => {
+        turn = 0;
+        _message.textContent = `Player ${_playerOne.playerShape}'s turn`;
+        _boardArray = ["", "", "", "", "", "", "", "", ""];
+
+        _gameWinner = '';
+
+        renderBoard();
+    }
+
+    const _binding = () => {
+        for(let i = 0; i < _boxes.length; i++){
+            _boxes[i].setAttribute('data-index', i);
+            
+            // eslint-disable-next-line no-undef
+            _boxes[i].addEventListener('click', _makeMove.bind(this, box_index = i));
+        }
+
+        _restartButton.addEventListener('click', _restartGame);
+    };
+
+    const init = () => {
+        _restartGame();
+        _binding();
+    }
+
+    return{init};
+
 })();
 
-const startButton = document.querySelector("#start-btn")
-startButton.addEventListener("click", ()=> {
-    //Game.start();
-})
+gameBoard.init();
+
